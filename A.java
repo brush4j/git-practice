b80b423eb60bee5733f806fac1a98f61a74b0407
Halfmoonly <liuyanoutsee@outlook.com>
2025-05-23 22:02:37 +0800

    main

---
+++ A.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author： hmly
+ * @date： 2025/5/23
+ * @description：
+ * @modifiedBy：
+ * @version: 1.0
+ */
+public class A {
+    private String str = "main";
+}
