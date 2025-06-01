d632ff3148b9d5983b82161accc795b159080503
brush4j <liuyanoutsee@outlook.com>
2025-06-01 12:32:25 +0800

    vscode

---
+++ src/main/java/org/brush4j/vsgit/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.brush4j.vsgit;
+
+/**
+ *
+ * @author hasee
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        IHandle handler = new UserHandleModify();
+        handler.handle1("hello handle");
+    }
+}
---
+++ src/main/java/org/brush4j/vsgit/UserHandleModify.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package org.brush4j.vsgit;
+
+/**
+ *
+ * @author hasee
+ */
+public class UserHandleModify implements IHandle{
+
+    @Override
+    public void handle1(String arg) {
+        System.out.println("UserHandleModify handle1: " + arg);
+    }
+
+}
