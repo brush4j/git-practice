9f1557df217fab1de755b080e7b7e58c54ae6438
brush4j <liuyanoutsee@outlook.com>
2025-06-01 11:24:18 +0800

    vscode

--- A.java
+++ A.java
@@ -1,3 +1,7 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -8,9 +12,21 @@
  * @version: 1.0
  */
 public class A {
-    private String str = "main commit";
-    private String str1 = "main commit.";
-    private String str2 = "main commit.";
-    private String str3 = "main commit.";
-    private String str4 = "main commit.";
+    /**
+     * Main method to demonstrate the use of HashMap.
+     */
+    public static void main(String[] args) {
+        System.out.println("...A.main()");
+        Map<String,String> map = new HashMap<>();
+        Map<String, String> d3 = map;
+        d3.put("1", "1");
+
+        // Attempting to retrieve a value using a key that does not exist in the map
+        String value = d3.get("2");
+        if (value == null) {
+            System.out.println("Key '2' does not exist in the map.");
+        } else {
+            System.out.println("Value for key '2': " + value);
+        }
+    }
 }
