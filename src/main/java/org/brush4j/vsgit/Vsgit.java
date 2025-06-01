a2f38b217d89c677f39e87287cba5f2613d0caa1
brush4j <liuyanoutsee@outlook.com>
2025-06-01 12:30:18 +0800

    vscode

---
+++ src/main/java/org/brush4j/vsgit/Vsgit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.brush4j.vsgit;
+
+/**
+ *
+ * @author hasee
+ */
+public class Vsgit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
