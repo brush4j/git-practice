8324724db42e5683b80ea205ec38c988791f3299
brush4j <liuyanoutsee@outlook.com>
2025-06-11 20:44:52 +0800

    Test.java sout main3

--- src/main/java/org/brush4j/vsgit/test/Test.java
+++ src/main/java/org/brush4j/vsgit/test/Test.java
@@ -13,5 +13,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World! main2");
+        System.out.println("Hello World! main3");
     }
 }
