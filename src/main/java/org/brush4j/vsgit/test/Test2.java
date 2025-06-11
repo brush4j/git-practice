98127e70911dd4f778bf389f650946deee846850
brush4j <liuyanoutsee@outlook.com>
2025-06-11 20:45:27 +0800

    add Test2.java

---
+++ src/main/java/org/brush4j/vsgit/test/Test2.java
@@ -0,0 +1,18 @@
+package org.brush4j.vsgit.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author： hmly
+ * @date： 2025/6/11
+ * @description：
+ * @modifiedBy：
+ * @version: 1.0
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World! main2");
+        System.out.println("Hello World! main3");
+    }
+}
