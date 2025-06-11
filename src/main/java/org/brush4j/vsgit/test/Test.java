5619e3133b47a571a003e85b2872285a499e199c
brush4j <liuyanoutsee@outlook.com>
2025-06-11 20:10:41 +0800

    Test.java

---
+++ src/main/java/org/brush4j/vsgit/test/Test.java
@@ -0,0 +1,16 @@
+package org.brush4j.vsgit.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author： hmly
+ * @date： 2025/6/11
+ * @description：
+ * @modifiedBy：
+ * @version: 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
