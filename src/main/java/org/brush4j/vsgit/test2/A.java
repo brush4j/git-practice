bffc8c80a50fbba5f237f4dca1d3e1ac3cf6d417
brush4j <liuyanoutsee@outlook.com>
2025-06-23 20:16:23 +0800

    A

---
+++ src/main/java/org/brush4j/vsgit/test2/A.java
@@ -0,0 +1,16 @@
+package org.brush4j.vsgit.test2;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author： hmly
+ * @date： 2025/6/23
+ * @description：
+ * @modifiedBy：
+ * @version: 1.0
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println("A");
+    }
+}
